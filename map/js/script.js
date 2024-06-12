929f7168d1dd0256fe93750ad07b42454e7d6532
EricHsu <Eric.hsu1@aecom.com>
2024-06-13 04:52:41 +0800

    upd: map positiion, imge, css tone

--- map/js/script.js
+++ map/js/script.js
@@ -57,12 +57,12 @@ let map = L.map("map", {
 map.setView([22.28036, 114.255152], 15);
 
 //>> Image Overlay
-let imageUrl = "./image/map_20240611.webp";
+let imageUrl = "./image/map_20240612_2.webp";
 let errorOverlayUrl = "https://cdn-icons-png.flaticon.com/512/110/110686.png";
 let altText = "image";
 let siteBounds = L.latLngBounds([
-  [22.251686, 114.226656],
-  [22.313793, 114.294548],
+  [22.250762, 114.224521],
+  [22.317078, 114.295996],
 ]);
 
 // zoom the map to the rectangle bounds
@@ -75,7 +75,7 @@ let imageOverlay = L.imageOverlay(imageUrl, siteBounds, {
 }).addTo(map);
 
 //>> Overlay Rectangle
-L.rectangle(siteBounds, { color: "#ffd96c", weight: 1 }).addTo(map);
+// L.rectangle(siteBounds, { color: "#ffd96c", weight: 1 }).addTo(map);
 
 L.control
   .attribution({
